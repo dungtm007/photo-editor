bc9fc2d42d547a7cfa6203a2d7eee98b4c1f41bc
DESKTOP-G6VSS41\Truong Dung <truongdung0502@gmail.com>
2018-02-06 12:41:33 -0600

    update currentFigure on reivew.js

--- src/main/webapp/resources/js/review.js
+++ src/main/webapp/resources/js/review.js
@@ -1,26 +1,21 @@
 "use strict";
-var currentFigure;
 var popup = {
   init: function() {
 	  $('figure').click(function(){
-		  currentFigure = this;
+		  popup.currentFigure = this;
 		  popup.open($(this));
 	  });
     
     $(document).on('click', ".popup #downloadImg", function(event) {
-    	//debugger;
     	event.stopPropagation(); // do not bubble it in DOM tree, so normal behavior of download button is maintained 
     })
     .on('click', '.popup #figPopup', function() {
-    	//debugger;
       return false; // prevent default and stop propagation
     })
     .on('click', '.popup', function(){
-    	//debugger;
       popup.close();
     }).on('click', '.popup #deleteImg', function(e, f){
     	var id = this.getAttribute("data-value");
-    	//console.log("Delete Image: "+ e);
     	popup.deleteImg(id);
     });
     
@@ -43,14 +38,12 @@ var popup = {
 			type: 'danger', allow_dismiss: true, mouse_over: "pause", delay: 1000, z_index: 10000
 		});
 	}
-    
   },
   open: function($figure) {
     var $popup = $('<div class="popup" />').appendTo($('body'));
     var $fig = $figure.clone().appendTo($('.popup'));
     $fig.attr("id", "figPopup");
     var $bg = $('<div class="bg" />').appendTo($('.popup'));
-    //var $close = $('<div class="close"><svg><use xlink:href="#close"></use></svg></div>').appendTo($fig);
     var $shadow = $('<div class="shadow" />').appendTo($fig);
     var $img = $('img', $fig);
     var src =  $img.attr('src');
@@ -75,7 +68,7 @@ var popup = {
 				type: 'success', allow_dismiss: true, mouse_over: "pause", delay: 1000
 			});
 			setTimeout(function(){
-				currentFigure.remove();
+				popup.currentFigure.remove();
 			}, 10);
 		})
 		.fail(function(xhr, textStatus, errorThrown ) {
