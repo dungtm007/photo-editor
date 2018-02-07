a02d4414fd4c3d70d66de6c6150e0883bc2308d2
DESKTOP-G6VSS41\Truong Dung <truongdung0502@gmail.com>
2018-02-07 15:43:57 -0600

    remove bg in review.js

--- src/main/webapp/resources/js/review.js
+++ src/main/webapp/resources/js/review.js
@@ -14,10 +14,11 @@ var popup = (function() {
         });
 
         $(document).on('click', ".popup #downloadImg", function(event) {
-                event.stopPropagation(); // do not bubble it in DOM tree, so normal behavior of download button is maintained 
+                event.stopPropagation(); // do not bubble it in DOM tree, so normal behavior of download button is maintained
             })
-            .on('click', '.popup #figPopup', function() {
-                return false; // prevent default and stop propagation
+            .on('click', '.popup #figPopup', function(event) {
+                //return false; // prevent default and stop propagation, don't close the pop-up
+            	event.stopPropagation();
             })
             .on('click', '.popup', function() {
                 self.close();
@@ -63,15 +64,11 @@ var popup = (function() {
         var popup = $('<div class="popup" />').appendTo($('body'));
         var fig = figure.clone().appendTo($('.popup'));
         fig.attr("id", "figPopup");
-        var bg = $('<div class="bg" />').appendTo($('.popup'));
         var shadow = $('<div class="shadow" />').appendTo(fig);
         var img = $('img', fig);
         var src = img.attr('src');
         img.attr("id", "imgPopup");
         shadow.css({
-            backgroundImage: 'url(' + src + ')'
-        });
-        bg.css({
             backgroundImage: 'url(' + src + ')'
         });
         setTimeout(function() {
